0a1a78e25568956b39f07da5ab6b2a8ac6ccf41d
Sujan Maharjan <sujanmaharjan1d@gmail.com>
2026-05-19 15:34:24 +0545

    home.py updated

---
+++ home.py
@@ -0,0 +1,20 @@
+temp=True
+while temp==True:
+
+    print("\nPress 1 to Play Games")
+    print("Press 2 to View Profile")
+    print("Press 3 to View Leaderboard")
+    print("Press 4 to Exit\n")
+    num=int(input("Enter your choice:"))
+
+    if num==1:
+        pass
+    elif num==2:
+        pass
+    elif num==3:
+        pass
+    elif num==4:
+        temp=False
+    else:
+        print("Invalid Input")
+        temp=False
