d3929b9facf31f9aab3a34790b1f7de6f5d52989
Sujan Maharjan <sujanmaharjan1d@gmail.com>
2026-05-21 17:35:02 +0545

    Updated guess_number.py

--- guess_number.py
+++ guess_number.py
@@ -1,11 +1,34 @@
-temp= True
-while temp==True:
-    user_number=int(input("Enter any number:"))
-    b=23
-    if b>user_number:
+import random
+temp = True
+tries = 0
+random_number = random.randint(1, 100)
+
+while temp == True:
+
+    user_number = int(input("Enter any number between 1 to 100:"))
+    tries += 1
+
+    if random_number > user_number:
         print("Your entered number is lower.")
-    elif user_number>b:
+
+    elif user_number > random_number:
         print("Your entered number is higher.")
+
     else:
-        print("You guess the correct number.")
+        print("\nYou guess the correct number.")
+        print("Total tries:",tries)
+
+        if tries == 1:
+            score = 100
+        elif tries == 2:
+            score = 80
+        elif tries == 3:
+            score = 60
+        elif tries == 4:
+            score = 40
+        else:
+            score = 20
+        
+        print("Your score is:", score)
+
         break
